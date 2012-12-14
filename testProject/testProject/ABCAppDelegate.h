69777f38cd9671f2ed42bf8c956d5040b149daee
ShivaniDave <shivani.dave@tatvasoft.com>
2012-12-14 15:10:07 +0530

    First Commit

---
+++ testProject/testProject/ABCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ABCAppDelegate.h
+//  testProject
+//
+//  Created by PCT147 on 14/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testProject/testProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testProject
+//
+//  Created by PCT147 on 14/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABCAppDelegate class]));
+    }
+}
